78bfbb155c8e37a06f2dba02a4e4b01214afeb5c
wStockhausen <william.stockhausen@noaa.gov>
2018-12-03 10:10:57 -0800

    Including nbproject folder in commit, now.

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,51 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=CLang-MacOSX
+CND_ARTIFACT_DIR_Debug=dist/Debug/CLang-MacOSX
+CND_ARTIFACT_NAME_Debug=tcsam02
+CND_ARTIFACT_PATH_Debug=dist/Debug/CLang-MacOSX/tcsam02
+CND_PACKAGE_DIR_Debug=dist/Debug/CLang-MacOSX/package
+CND_PACKAGE_NAME_Debug=tcsam02.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/CLang-MacOSX/package/tcsam02.tar
+# Release configuration
+CND_PLATFORM_Release=CLang-MacOSX
+CND_ARTIFACT_DIR_Release=dist/Release/CLang-MacOSX
+CND_ARTIFACT_NAME_Release=tcsam02
+CND_ARTIFACT_PATH_Release=dist/Release/CLang-MacOSX/tcsam02
+CND_PACKAGE_DIR_Release=dist/Release/CLang-MacOSX/package
+CND_PACKAGE_NAME_Release=tcsam02.tar
+CND_PACKAGE_PATH_Release=dist/Release/CLang-MacOSX/package/tcsam02.tar
+# Debug-MacOSX-ADMBv12 configuration
+CND_PLATFORM_Debug-MacOSX-ADMBv12=GNU-MacOSX
+CND_ARTIFACT_DIR_Debug-MacOSX-ADMBv12=dist/Debug-MacOSX-ADMBv12/GNU-MacOSX
+CND_ARTIFACT_NAME_Debug-MacOSX-ADMBv12=tcsam02
+CND_ARTIFACT_PATH_Debug-MacOSX-ADMBv12=dist/Debug-MacOSX-ADMBv12/GNU-MacOSX/tcsam02
+CND_PACKAGE_DIR_Debug-MacOSX-ADMBv12=dist/Debug-MacOSX-ADMBv12/GNU-MacOSX/package
+CND_PACKAGE_NAME_Debug-MacOSX-ADMBv12=tcsam02.tar
+CND_PACKAGE_PATH_Debug-MacOSX-ADMBv12=dist/Debug-MacOSX-ADMBv12/GNU-MacOSX/package/tcsam02.tar
+# MacOSX-ADMBv12 configuration
+CND_PLATFORM_MacOSX-ADMBv12=GNU-MacOSX
+CND_ARTIFACT_DIR_MacOSX-ADMBv12=dist/MacOSX-ADMBv12/GNU-MacOSX
+CND_ARTIFACT_NAME_MacOSX-ADMBv12=tcsam02
+CND_ARTIFACT_PATH_MacOSX-ADMBv12=dist/MacOSX-ADMBv12/GNU-MacOSX/tcsam02
+CND_PACKAGE_DIR_MacOSX-ADMBv12=dist/MacOSX-ADMBv12/GNU-MacOSX/package
+CND_PACKAGE_NAME_MacOSX-ADMBv12=tcsam02.tar
+CND_PACKAGE_PATH_MacOSX-ADMBv12=dist/MacOSX-ADMBv12/GNU-MacOSX/package/tcsam02.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
